16a4433bec622a5db039bc500249cdc80cee61ad
shenalachintha <achinthas666@gmail.com>
2025-12-31 21:03:58 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+const CONNECTION_URL = "mongodb+srv://user:20020209@memodb.pin3ew0.mongodb.net/?appName=MemoDb";
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,useUnifiedTopology: true
+    }).then
+    (() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+mongoose.set("useFindAndModify", false);
