09318032c4ba709b5c0fe93f95d2505f1feb283b
shenalachintha <achinthas666@gmail.com>
2025-12-31 21:08:41 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -9,9 +9,7 @@ app.use(cors());
 const CONNECTION_URL = "mongodb+srv://user:20020209@memodb.pin3ew0.mongodb.net/?appName=MemoDb";
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL, {
-    useNewUrlParser: true,useUnifiedTopology: true
-    }).then
-    (() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
-mongoose.set("useFindAndModify", false);
+  .connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+// Removed deprecated mongoose option `useFindAndModify` (no longer supported in Mongoose v6+)
