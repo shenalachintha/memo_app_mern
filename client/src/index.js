7acd776aae87aef4469b078d5df17a4a12b797db
shenalachintha <achinthas666@gmail.com>
2026-01-01 11:19:28 +0530

    new commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {provider} from 'react-redux';
+import {createStore,applyMiddleware,compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+
+const store=createStore(  
+    reducers,
+    compose(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-    <App />,
+        <App />,
     document.getElementById("root")
 );
