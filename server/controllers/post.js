1ebffc8025fd088b1db792563357e63ca4a4475b
shenalachintha <achinthas666@gmail.com>
2026-01-01 09:56:40 +0530

    implement createpost method

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -10,7 +10,15 @@ const getposts =  async (req, res) => {
         res.status(404).json({message:error.message});
     }
 }
-const createPost = (req, res) => {
-    res.send('Create Post route is working!');
+const createPost =  async (req, res) => {
+    const post=req.body
+    const newPost= new PostMessage(post)
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+        
+    } catch (error) {
+        res.status(409).json({message:error.message});      
+    }
 }
-export { getposts };
+export { getposts,createPost };
