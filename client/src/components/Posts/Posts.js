4ce6851b2402a334a60f9cf65a1492683b0bf122
shenalachintha <achinthas666@gmail.com>
2026-01-02 23:22:22 +0530

    new commit

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Post from "./Post/Post.js";
+import { useSelector } from "react-redux";
+import{usestyles}  from './styles.js';
 
 const Posts=()=>{
+    const posts = useSelector((state) => state.posts);
+    const classes=usestyles();
+    console.log(posts);
+
     return(
         <div>
             <>
