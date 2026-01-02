0aff39b0db72b1b535f87f4dcdc2edf95679062f
shenalachintha <achinthas666@gmail.com>
2026-01-03 00:16:58 +0530

    new commit

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,23 +1,22 @@
 import React from "react";
 import Post from "./Post/Post.js";
 import { useSelector } from "react-redux";
-import{usestyles}  from './styles.js';
+import useStyles from "./styles.js";
 
-const Posts=()=>{
+const Posts = () => {
     const posts = useSelector((state) => state.posts);
-    const classes=usestyles();
+    const classes = useStyles();
     console.log(posts);
 
-    return(
-        <div>
+    return (
+        <div className={classes.mainContainer}>
             <>
-             <h1>Posts Component</h1>
-            <Post/>
-            <Post/>
-            <Post/>
+                <h1>Posts Component</h1>
+                <Post />
+                <Post />
+                <Post />
             </>
-           
         </div>
-    )   
-}
+    );
+};
 export default Posts;
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import {provider} from 'react-redux';
-import {createStore,applyMiddleware,compose} from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+import reducers from "./reducers";
 
-
-const store=createStore(  
+const store = createStore(
     reducers,
     compose(applyMiddleware(thunk))
 );
 
-
 ReactDOM.render(
-    <provider store={store}>
-     <App />
-    </provider>,  
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("root")
 );
