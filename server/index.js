ffa8cf7d30c4a9ef1e51bc160ecf1438c5ca4771
shenalachintha <achinthas666@gmail.com>
2026-01-04 22:59:15 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 const app = express();
 import postRoutes from './routes/posts.js';
 
+// Load environment variables (e.g., CONNECTION_URL)
+dotenv.config();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
-const CONNECTION_URL = "mongodb+srv://user:20020209@memodb.pin3ew0.mongodb.net/?appName=MemoDb";
+const CONNECTION_URL = process.env.CONNECTION_URL || "mongodb://127.0.0.1:27017/memo";
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL)
