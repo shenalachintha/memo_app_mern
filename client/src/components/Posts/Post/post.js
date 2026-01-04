9e7df16d44582a59842191d64618b9a6304ef3da
shenalachintha <achinthas666@gmail.com>
2026-01-04 22:33:31 +0530

    create post card frontend

--- client/src/components/Posts/Post/post.js
+++ client/src/components/Posts/Post/post.js
@@ -1,9 +1,42 @@
-import React from "react";
-const Post=()=>{
+import React, { use } from "react";
+import { Card,CardActions,CardContent,CardMedia,Button,Typography } from "@material-ui/core";
+import {ThumbUpAlt,Delete,MoreHoriz} from "@material-ui/icons";
+import useStyles from "./styles.js";
+const Post=({post})=>{
+    const classes=useStyles();
     return(
-        <div>
-            <h1>Post Component</h1>
-        </div>
-    )       
+       <Card className={classes.card} > 
+         <CardMedia className={classes.cardMedia} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{new Date(post.createdAt).toDateString()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color:'white'}} size="small" >
+                    <MoreHoriz fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=>`#${tag} `)}</Typography>   
+            </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={()=>{}}>
+                    <ThumbUpAlt fontSize="small" />
+                    &nbsp; Like &nbsp; {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={()=>{}}>
+                    <Delete fontSize="small" />
+                    Delete
+                </Button>
+            </CardActions>
+       </Card>
+
+
+    
+      )            
 }
 export default Post;
