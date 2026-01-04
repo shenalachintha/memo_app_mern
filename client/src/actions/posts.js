e6ef63b794cf5b37ab25e349c7d000074e7f9074
shenalachintha <achinthas666@gmail.com>
2026-01-04 21:39:00 +0530

    get create post data through api

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -10,3 +10,13 @@ import * as api from '../api/index.js';
         console.log(error.message);        
     }
 }
+    export const createPost=(post)=>async(dispatch)=>{
+        try {
+            const {data}=await api.createPost(post);
+            dispatch({type:'CREATE',payload:data});
+            
+        } catch (error) {
+            console.log(error.message);
+            
+        }
+    }
