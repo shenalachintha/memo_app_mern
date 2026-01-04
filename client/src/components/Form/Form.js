38f176308793853d81d42f83c1da44b622cc41b4
shenalachintha <achinthas666@gmail.com>
2026-01-04 21:42:40 +0530

    action dispatch in form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,9 +2,14 @@ import React ,{useState}from "react";
 import Usestyles from './styles.js';
 import { TextField,Button,Typography,Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts.js";
 const Form=()=>{
     const classes=Usestyles();
-    const handleSubmit=()=>{
+    const dispatch=useDispatch();
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        dispatch(createPost(postData));
     }
     const clear=()=>{
     }
