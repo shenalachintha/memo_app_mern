ff3329d89d27f226a454d5fea2a97ed37a4b267f
shenalachintha <achinthas666@gmail.com>
2026-01-05 23:54:49 +0530

    new commit

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -20,3 +20,11 @@ import * as api from '../api/index.js';
             
         }
     }
+    export const updatePost=(id,post)=>async(dispatch)=>{
+        try {
+            const {data}=await api.updatePost(id,post);
+            dispatch({type:'UPDATE',payload:data});
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
