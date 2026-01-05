1e6757758a93f5827766a7b77639098d229e7661
shenalachintha <achinthas666@gmail.com>
2026-01-05 23:29:06 +0530

    create patch(update) route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getposts,createPost} from '../controllers/post.js';
+import { getposts,createPost,updatePost} from '../controllers/post.js';
 
 const router = express.Router();
 
 router.get('/',getposts);
 router.post('/',createPost)
+router.patch('/:id',updatePost );
 
 export default router;
