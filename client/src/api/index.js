80c3e030a8a89f1acd8d4ac7408bc04a28e94f23
shenalachintha <achinthas666@gmail.com>
2026-01-07 22:57:52 +0530

    update api for delete

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,3 +4,4 @@ const fetchPosts=()=>axios.get(url);
 export {fetchPosts};
 export const createPost=(newPost)=>axios.post(url,newPost);
 export const updatePost=(id,updatedPost)=>axios.patch(`${url}/${id}`,updatedPost);
+export const deletePost=(id)=>axios.delete(`${url}/${id}`);
